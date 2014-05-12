08e1873f9560add6b78e56e3fe23dde0edb1caf8
Michael Rice <michael.rice@rackspace.com>
2014-05-11 19:02:06 -0500

    more updates

--- JschSsh2GrailsPlugin.groovy
+++ JschSsh2GrailsPlugin.groovy
@@ -1,3 +1,10 @@
+import grails.util.Environment
+import org.codehaus.groovy.grails.commons.GrailsApplication
+
+import groovy.util.ConfigObject
+import groovy.util.ConfigSlurper
+
+
 /**
  * Grails plugin providing the JSCH lib with builder for
  * many options making implementation and use easy from
@@ -27,12 +34,30 @@ class JschSsh2GrailsPlugin {
     def issueManagement = [ system: "GITHUB", url: "https://github.com/michaelrice/jsch-ssh2/issues" ]
     def scm = [ url: "https://github.com/michaelrice/jsch-ssh2" ]
 
-    def doWithSpring = {
-        // TODO Will this be needed like in the other plugin?
-    }
-
     def onShutdown = { event ->
         // TODO Seems like we should make sure we didnt leave any connections open.
         // Need to find out if lib handles that or do we need to?
     }
+
+    def doWithSpring = {
+        loadDefaultConfig(application)
+    }
+
+    def onConfigChange = {
+        loadDefaultConfig(application)
+    }
+
+    /**
+     * Merges plugin config with host app config, but allowing customization
+     * @param app
+     */
+    private void loadDefaultConfig(GrailsApplication app) {
+        GroovyClassLoader classLoader = new GroovyClassLoader(getClass().classLoader)
+
+        ConfigObject defaultConfig = new ConfigSlurper(Environment.current.name).parse(classLoader.loadClass('DefaultJschSsh2Config'))
+
+        defaultConfig.merge(app.config)
+        app.config.merge(defaultConfig)
+    }
+
 }
---
+++ grails-app/conf/DefaultJschSsh2Config.groovy
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael Rice
+ * Twitter: @errr_
+ * Website: http://www.errr-online.com/
+ * Github: https://github.com/michaelrice
+ * Date: 5/11/2014
+ * Time: 4:05 PM
+ * Licenses: MIT http://opensource.org/licenses/MIT
+ */
+
+jschSsh2 {
+    user = null
+    password = null
+    keyFile = null
+    keyFilePassword = null
+    terminal = "vt220"
+    port = 22
+}
